6e4e092939e72b28a13271c8f457423424472d9b
thanujav16126-ux <thanujav16126@gmail.com>
2026-03-11 13:55:30 +0530

    Update destination.js

--- Js/destination.js
+++ Js/destination.js
@@ -1,1 +1,24 @@
+function nextPage(){
 
+let boarding=document.getElementById("boardingPoint").value;
+let destination=document.querySelector('input[name="location"]:checked');
+
+if(boarding==""){
+document.getElementById("popup").style.display="flex";
+document.querySelector("#popup p").innerText="Please select boarding point";
+return;
+}
+
+if(!destination){
+document.getElementById("popup").style.display="flex";
+document.querySelector("#popup p").innerText="Please select destination";
+return;
+}
+
+window.location.href="seats.html";
+
+}
+
+function closePopup(){
+document.getElementById("popup").style.display="none";
+}
